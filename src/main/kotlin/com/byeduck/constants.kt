670316776ab24ba330313ef30f65300f432beb2c
byeduck <mateuszbajdak@gmail.com>
2021-07-09 18:16:59 +0200

    init

---
+++ src/main/kotlin/com/byeduck/constants.kt
@@ -0,0 +1,6 @@
+package com.byeduck
+
+const val BUGS_FILE_PATH = "bugs.csv"
+const val DEVICES_FILE_PATH = "devices.csv"
+const val TESTER_DEVICE_FILE_PATH = "tester_device.csv"
+const val TESTERS_FILE_PATH = "testers_file_path"
---
+++ src/main/kotlin/com/byeduck/main.kt
@@ -0,0 +1,5 @@
+package com.byeduck
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}

08cd0fdb7d6267366ffd60c56903af32ce3244d2
byeduck <mateuszbajdak@gmail.com>
2021-07-10 01:59:36 +0200

    chore: maven exec config

--- src/main/kotlin/com/byeduck/main.kt
+++ src/main/kotlin/com/byeduck/main.kt
@@ -2,13 +2,22 @@ package com.byeduck
 
 import com.byeduck.csv.DefaultContextFileNamesProvider
 
+// mvn package exec:java -Dexec.args="JP,GP iPhone_4,iPhone_4S"
 fun main(args: Array<String>) {
     val defaultCountrySearchCriteria = "ALL"
     val defaultDeviceSearchCriteria = "ALL"
-    val countrySearchCriteria = args.getOrElse(1) { defaultCountrySearchCriteria }
-    val deviceSearchCriteria: String = args.getOrElse(2) { defaultDeviceSearchCriteria }
+    val criteria = args[0].split(" ")
+    val countrySearchCriteria = criteria.getOrElse(0) { defaultCountrySearchCriteria }.sanitizeInputCriteria()
+    val deviceSearchCriteria: String = criteria.getOrElse(1) { defaultDeviceSearchCriteria }.sanitizeInputCriteria()
+    println("Executing matching for the following criteria: $countrySearchCriteria | $deviceSearchCriteria")
     val testingMatcher = TestingMatcher(DefaultContextFileNamesProvider())
     val testersWithRanks = testingMatcher.match(countrySearchCriteria, deviceSearchCriteria)
     val result = testersWithRanks.joinToString("\n") { it.toString() }
-    print("Results:\n$result")
+    println("Results:\n$result")
 }
+
+fun String.sanitizeInputCriteria() = this
+    .replace('_', ' ')
+    .removeSurrounding("\"")
+    .removeSurrounding("'")
+    .trim()

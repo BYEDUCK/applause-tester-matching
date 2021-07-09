f6250a1a75eacc851f4fc153afb510d6e07328e6
byeduck <mateuszbajdak@gmail.com>
2021-07-09 21:35:38 +0200

    refactor: extract row mappers to separate methods

--- src/main/kotlin/com/byeduck/context/ApplicationContextProvider.kt
+++ src/main/kotlin/com/byeduck/context/ApplicationContextProvider.kt
@@ -24,41 +24,49 @@ class ApplicationContextProvider {
                 return
             }
             val csvReader: CsvReader = DefaultCsvReader()
-            val bugs = csvReader.readAll(BUGS_FILE_NAME) {
-                Bug(
-                    it["bugId"]?.toInt() ?: throw FieldNotFoundException("bugId"),
-                    it["deviceId"]?.toInt() ?: throw FieldNotFoundException("deviceId"),
-                    it["testerId"]?.toInt() ?: throw FieldNotFoundException("testerId")
-                )
-            }
-            val devices = csvReader.readAll(DEVICES_FILE_NAME) {
-                Device(
-                    it["deviceId"]?.toInt() ?: throw FieldNotFoundException("deviceId"),
-                    it["description"] ?: throw FieldNotFoundException("description")
-                )
-            }.associateBy { it.id }
-            val testers = csvReader.readAll(TESTERS_FILE_NAME) {
-                val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                Tester(
-                    it["testerId"]?.toInt() ?: throw FieldNotFoundException("testerId"),
-                    it["firstName"] ?: throw FieldNotFoundException("firstName"),
-                    it["lastName"] ?: throw FieldNotFoundException("lastName"),
-                    it["country"] ?: throw FieldNotFoundException("country"),
-                    LocalDateTime.parse(it["lastLogin"], dateFormatter) ?: throw FieldNotFoundException("lastLogin")
-                )
-            }
-            val testerDevices = csvReader.readAll(TESTER_DEVICE_FILE_NAME) {
-                TesterDevice(
-                    it["testerId"]?.toInt() ?: throw FieldNotFoundException("testerId"),
-                    it["deviceId"]?.toInt() ?: throw FieldNotFoundException("deviceId")
-                )
-            }
-            val devicesIdsByTesterId = testerDevices
+            val bugs = csvReader.readAll(BUGS_FILE_NAME, this::bugCsvMapper)
+            val devices = csvReader.readAll(DEVICES_FILE_NAME, this::deviceCsvMapper)
+                .associateBy { it.id }
+            val testers = csvReader.readAll(TESTERS_FILE_NAME, this::testersMapper)
+            val devicesIdsByTesterId = csvReader.readAll(TESTER_DEVICE_FILE_NAME, this::testerDeviceMapper)
                 .groupBy(TesterDevice::testerId)
                 .mapValues { it.value.map(TesterDevice::deviceId) }
             applicationContext = ApplicationContext(
                 bugs,
                 testers.map { TesterWithDevices.merge(it, devices, devicesIdsByTesterId) }
+            )
+        }
+
+        private fun bugCsvMapper(rowData: Map<String, String>): Bug = rowData.let {
+            Bug(
+                it["bugId"]?.toInt() ?: throw FieldNotFoundException("bugId"),
+                it["deviceId"]?.toInt() ?: throw FieldNotFoundException("deviceId"),
+                it["testerId"]?.toInt() ?: throw FieldNotFoundException("testerId")
+            )
+        }
+
+        private fun deviceCsvMapper(rowData: Map<String, String>): Device = rowData.let {
+            Device(
+                it["deviceId"]?.toInt() ?: throw FieldNotFoundException("deviceId"),
+                it["description"] ?: throw FieldNotFoundException("description")
+            )
+        }
+
+        private fun testersMapper(rowData: Map<String, String>): Tester = rowData.let {
+            val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            Tester(
+                it["testerId"]?.toInt() ?: throw FieldNotFoundException("testerId"),
+                it["firstName"] ?: throw FieldNotFoundException("firstName"),
+                it["lastName"] ?: throw FieldNotFoundException("lastName"),
+                it["country"] ?: throw FieldNotFoundException("country"),
+                LocalDateTime.parse(it["lastLogin"], dateFormatter) ?: throw FieldNotFoundException("lastLogin")
+            )
+        }
+
+        private fun testerDeviceMapper(rowData: Map<String, String>): TesterDevice = rowData.let {
+            TesterDevice(
+                it["testerId"]?.toInt() ?: throw FieldNotFoundException("testerId"),
+                it["deviceId"]?.toInt() ?: throw FieldNotFoundException("deviceId")
             )
         }
     }
